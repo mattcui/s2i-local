0dceedc6d7e85d878ac84eecfc942d7de3d9e747
XUE XIANG CUI <cuixuex@cn.ibm.com>
2020-12-29 14:03:08 +0800

    Leverage mink bundle to make source code image (#3)

--- cmd/build.go
+++ cmd/build.go
@@ -16,31 +16,116 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/mattmoor/mink/pkg/kontext"
+	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
 
-// buildCmd represents the build command
-var buildCmd = &cobra.Command{
-	Use:   "build",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+func NewBuildCommand(ctx context.Context) *cobra.Command {
+	opts := &BuildOptions{
+		ctx: ctx,
+	}
+
+	// buildCmd represents the build command
+	buildCmd := &cobra.Command{
+		Use:   "build",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("build called")
-	},
+to quickly creatree a Cobra application.`,
+		PreRunE: opts.Validate,
+		RunE:    opts.Execute,
+	}
+
+	opts.AddFlags(buildCmd)
+
+	return buildCmd
 }
 
 func init() {
-	rootCmd.AddCommand(buildCmd)
+	rootCmd.AddCommand(NewBuildCommand(context.Background()))
+}
 
-	buildCmd.Flags().StringP("name", "n", "", "Set your name")
-	buildCmd.Flags().StringP("directory", "d","", "The directory to bundle up")
-	buildCmd.Flags().StringP("image", "i","", "The image to generate and publish")
-	buildCmd.Flags().StringP("strategy", "s","", "The build strategy to build the image")
+// BuildOptions implements Interface for the `kn im build` command.
+type BuildOptions struct {
+	ctx context.Context
+
+	// ImageName is the string name of the bundle image to which we should publish things.
+	ImageName string
+
+	// Directory is the string containing the directory to bundle.
+	// This option signals "kontext mode".
+	Directory string
+
+	Strategy string
+
+	// tag is the processed version of ImageName that is populated while validating it.
+	tag name.Tag
+}
+
+// Validate implements Interface
+func (opts *BuildOptions) Validate(cmd *cobra.Command, args []string) error {
+	_ = viper.BindPFlags(cmd.Flags())
+	opts.ImageName = viper.GetString("image")
+
+	// See if we're in "kontext mode"
+	opts.Directory = viper.GetString("directory")
+	if opts.Directory == "" {
+		opts.Directory = "."
+	}
+
+	opts.Strategy = viper.GetString("strategy")
+
+	if opts.ImageName == "" {
+		return errors.New("image url is required")
+	} else if tag, err := name.NewTag(opts.ImageName, name.WeakValidation); err != nil {
+		return err
+	} else {
+		opts.tag = tag
+	}
+
+	return nil
+}
+
+// AddFlags implements Interface
+func (opts *BuildOptions) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("name", "n", "", "The build name")
+	cmd.Flags().StringP("directory", "d", ".", "The directory to bundle up")
+	cmd.Flags().StringP("image", "i", "", "The image to generate and publish")
+	cmd.Flags().StringP("strategy", "s", "local", "The build strategy to build the image")
+
+	_ = cmd.MarkPersistentFlagRequired("name")
+	_ = cmd.MarkPersistentFlagRequired("image")
+}
+
+func (opts *BuildOptions) bundle(ctx context.Context) (name.Digest, error) {
+
+	return kontext.Bundle(ctx, opts.Directory, opts.tag)
+
+}
+
+func (opts *BuildOptions) Execute(cmd *cobra.Command, args []string) error {
+	if len(args) != 0 {
+		return errors.New("'im bundle' does not take any arguments")
+	}
+
+	digest, err := opts.bundle(opts.GetContext(cmd))
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(cmd.OutOrStdout(), "%s\n", digest.String())
+	return nil
+}
+
+// GetContext implements Interface
+func (opts *BuildOptions) GetContext(cmd *cobra.Command) context.Context {
+	return opts.ctx
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.ibm.com/cuixuex/s2i-local
 go 1.15
 
 require (
+	github.com/google/go-cmp v0.5.4 // indirect
+	github.com/google/go-containerregistry v0.3.0
+	github.com/mattmoor/mink v0.19.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3 // indirect
+	golang.org/x/text v0.3.4 // indirect
 )
+
+replace k8s.io/client-go => k8s.io/client-go v0.18.8
